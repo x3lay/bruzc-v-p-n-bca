8f3e4d15b5d0b47fff85463b95f688187f9d6d80
x3lay <x3layka@gmail.com>
2025-05-28 12:12:22 +0300

    Update vite.config.ts for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-   import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
-   export default defineConfig({
-     plugins: [react()],
-     base: '/brutovpn/' // Укажите имя репозитория для корректных путей
-   });
+export default defineConfig({
+  plugins: [react()],
+  base: '/' // Для Netlify используем корневой путь
+});
